e57d2bc22b25dc291bfbb10bb7ecfa8b002a4579
leovct <l009.vincent@gmail.com>
2024-02-23 11:47:42 +0100

    fix: warnings in script

--- src/heimdall/scripts/start.sh
+++ src/heimdall/scripts/start.sh
@@ -2,16 +2,16 @@
 # Generate Heimdall genesis file and start the validator node.
 
 # Generate dummy configuration files (including genesis).
-heimdalld init --chain-id {{.CHAIN_ID}} --home {{.DATA_PATH}}
+heimdalld init --chain-id "{{.CHAIN_ID}}" --home "{{.DATA_PATH}}"
 
 # Generate the validator private key.
-polycli wallet inspect --addresses {{.VALIDATORS_COUNT}} --mnemonic '{{.MNEMONIC}}' | jq -r '.Addresses[{{.NODE_ID}}] | .HexPrivateKey' > {{.DATA_PATH}}/data/private_key.txt
-pushd {{.DATA_PATH}}/config
+polycli wallet inspect --addresses "{{.VALIDATORS_COUNT}}" --mnemonic "{{.MNEMONIC}}" | jq -r ".Addresses[{{.NODE_ID}}] | .HexPrivateKey" > "{{.DATA_PATH}}/data/private_key.txt"
+pushd "{{.DATA_PATH}}/config" || exit
 heimdallcli generate-validatorkey --home . "$(cat ../data/private_key.txt)"
 
 # Generate the final genesis file.
-heimdalld init --chain-id {{.CHAIN_ID}} --home {{.DATA_PATH}} --id {{.NODE_ID}} --overwrite-genesis
+heimdalld init --chain-id "{{.CHAIN_ID}}" --home "{{.DATA_PATH}}" --id "{{.NODE_ID}}" --overwrite-genesis
 
 # Start the validator node.
 # TODO: Is it necessary to specify --all and also --bridge?
-heimdalld start --all --amqp_url {{.AMQP_URL}} --bridge --rest-server
+heimdalld start --all --amqp_url "{{.AMQP_URL}}" --bridge --rest-server
